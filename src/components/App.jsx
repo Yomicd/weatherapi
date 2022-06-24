38efb70cff6040772234c04854c9f80e04162e2c
Ashley Douglas <douglasashleyc@gmail.com>
2022-06-24 11:28:15 -0400

    changes to component view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
 import React from "react";
 
-import Home from "."
+import Home from "./Home"
+
+const App = () => {
+    return(
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //our app
+import App from "./components/App"
 
 
 
 //export app
+ReactDOM.render(
+    <App />, document.querySelector("#root")
+);
